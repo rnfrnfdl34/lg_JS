0ec1228a5f9cdc7f7d071c9033db7a54f5a4a20c
Tae0074 <62372502+rnfrnfdl34@users.noreply.github.com>
2024-04-01 16:37:29 +0900

    24/04/01
    todo start

--- js/js/background.js
+++ js/js/background.js
@@ -1,8 +1,14 @@
 const images = [
     "0.jpg",
     "1.jpg",
-    "2.jps",
+    "2.jpg",
     "3.jpg"
 ];
 
 const choosenImage = images[Math.floor(Math.random()*images.length)];
+
+const bgImage = document.createElement("img");
+
+bgImage.src = `img/${choosenImage}`;
+document.body.appendChild(bgImage)
+console.log(bgImage)
