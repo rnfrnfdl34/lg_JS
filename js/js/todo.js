51e750997db93a0801e3fb94668972afb0c250e1
Tae0074 <62372502+rnfrnfdl34@users.noreply.github.com>
2024-04-04 18:04:47 +0900

    Update todo.js

--- js/js/todo.js
+++ js/js/todo.js
@@ -1,23 +1,42 @@
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
+const toDoForm = document.getElementById("todo-form");
 
 const TODOS_KEY = "todos";
 
-const toDos = [];
+let toDos = [];
 
-function saveToDos() {
-  localStorage.setItem("todos", JSON.stringify(toDos));
-  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
-}
+
 
 function deleteToDo(event) {
   const li = event.target.parentElement;
   li.remove();
+  toDos = toDos.filter(toDo=>toDo.id!==parseInt(li.id));
+  //function noName(todo){
+  //  return todo.id !== li.id }
+  saveToDos()
+  
 }
+
+
+function handleToDoSubmit(event) {
+  event.preventDefault();
+  const newTodo = toDoInput.value;
+  toDoInput.value = "";
+  const newTodoObj = {
+    text : newTodo,
+    id : Date.now()
+  }
+  toDos.push(newTodoObj);
+  paintToDo(newTodoObj);
+  saveToDos();
+}
+
 function paintToDo(newTodo) {
   const li = document.createElement("li");
-  const span = document.createElement("span");
-  span.innerText = newTodo;
+  li.id=newTodo.id;
+  const span = document.createElement("span"); 
+  span.innerText = newTodo.text;
   const button = document.createElement("button");
   button.innerText = "❌";
   button.addEventListener("click", deleteToDo);
@@ -25,13 +44,9 @@ function paintToDo(newTodo) {
   li.appendChild(button);
   toDoList.appendChild(li);
 }
-function handleToDoSubmit(event) {
-  event.preventDefault();
-  const newTodo = toDoInput.value;
-  toDoInput.value = "";
-  toDos.push(newTodo);
-  paintToDo(newTodo);
-  saveToDos();
+
+function saveToDos() {
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
@@ -40,5 +55,12 @@ const savedToDos = localStorage.getItem(TODOS_KEY);
 
 if (savedToDos !== null) {
   const parsedToDos = JSON.parse(savedToDos);
-  parsedToDos.forEach((item) => console.log("this is the turn of ", item));
+  toDos = parsedToDos;
+  parsedToDos.forEach(paintToDo);
+  //forEach 함수는 paintToDo 함수를 parsedToDos 배열의 요소마다 실행
+  //parsedToDos.forEach((item) => console.log("this is the turn of ", item));
+  //forEach: array에 있는 item에 대해 실행
+  // Arrow function(=>) 는
+  //function sayHello(item){
+  //  console.log("this is the turn of",item)}와 동일
 }
